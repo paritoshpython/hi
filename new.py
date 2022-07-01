b83b7fc536347fcb619754582970265ead4814ab
paritoshpython <paritoshpersonal90@gmail.com>
2022-07-01 09:30:05 +0530

    Initial Commit

---
+++ new.py
@@ -0,0 +1,20 @@
+class Name:
+    def __init__(self):
+        self.name="paritosh"
+        self.surname="kapadiya"
+        print("name:",self.name)
+
+    def show(self):
+        ok=self.name
+        return ("ok:",ok)   
+
+
+a=Name()
+
+class b(Name):
+    def __init__(self):
+        print("aaaaaaaa")
+
+c=b()
+print(c)
+
